07506d0845cb038facfc897ba2157a06011e4bc2
chautruongphat123 <chautruongphat123@gmail.com>
2020-12-22 08:33:24 +0700

    first code

---
+++ XayDungUngDungWebVoiASPNetCore/XayDungUngDungWebVoiASPNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XayDungUngDungWebVoiASPNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
